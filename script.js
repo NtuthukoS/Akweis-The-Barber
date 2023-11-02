b21a4931de727450a84e574594bfeaf1162f866d
NtuthukoS <66573876+NtuthukoS@users.noreply.github.com>
2023-11-02 15:18:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,38 @@
-// JavaScript to handle dropdown selection and auto-fill calendar
-const openCalendarButtons = document.querySelectorAll(".open-calendar");
-const serviceSelect = document.getElementById("service");
-
-openCalendarButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-        const selectedBookingType = event.target.getAttribute("data-booking-type");
-        let serviceOptions = []; // Initialize service options
-
-        if (selectedBookingType === "in-store") {
-            // In-Store service options
-            serviceOptions = [
-                "Fade (R100)",
-                "Shave (R80)",
-                "Dye (R120)",
-                // Add other service options for in-store
-            ];
-        } else if (selectedBookingType === "house-call") {
-            // House Call service options (prices are at least 4x)
-            serviceOptions = [
-                "Fade (R400+)",
-                "Shave (R320+)",
-                "Dye (R480+)",
-                // Add other service options for house call
-            ];
+// Function to generate available time slots
+function generateTimeSlots() {
+    const timeSelect = document.getElementById("time");
+    
+    for (let hour = 10; hour <= 18; hour++) {
+        for (let minutes = 0; minutes < 60; minutes += 30) {
+            const formattedTime = `${hour < 10 ? "0" : ""}${hour}:${minutes === 0 ? "00" : minutes}`;
+            const option = document.createElement("option");
+            option.value = formattedTime;
+            option.text = formattedTime;
+            timeSelect.appendChild(option);
         }
+    }
+}
 
-        // Populate the service dropdown based on the selected booking type
-        populateServiceOptions(serviceOptions);
-    });
-});
-
-function populateServiceOptions(options) {
-    const serviceSelect = document.getElementById("service");
-    serviceSelect.innerHTML = "";
-
-    options.forEach((option) => {
-        const optionElement = document.createElement("option");
-        optionElement.text = option;
-        serviceSelect.add(optionElement);
-    });
-
-
+// Function to close the overlay
 function closeOverlay() {
     const overlay = document.getElementById("overlay");
     overlay.style.display = "none";
 }
+
+// Function to open the overlay
+function openOverlay() {
+    const overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+    generateTimeSlots();
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const openCalendarButtons = document.querySelectorAll(".open-calendar");
+
+    openCalendarButtons.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            e.preventDefault();
+            openOverlay();
+        });
+    });
+});
